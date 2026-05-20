fd2f72b5839d6d870776ba7188581dbf43514e6e
boxtersh <boxtersh@yandex.ru>
2026-05-20 21:39:15 +0300

    add file config_example.py

---
+++ config_example.py
@@ -0,0 +1,11 @@
+class Settings:
+    HOST = "localhost"
+    USER = "your_username"
+    PASSWORD = "your_password"
+    DATABASE = "your_database_name"
+    PORT = "your_number_port"
+    TOKEN = 'your_TOKEN'
+
+
+settings = Settings()
+
